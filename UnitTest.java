40380c96916423a43249c44914cd5038974e2a16
dvanleusen <danvanleusen@yahoo.co.uk>
2016-10-14 04:50:11 -0600

    Unit Test after 2nd refactoring
    Revised tests after refactored the data class smell; added tests for
    human and computer constructors, accessors, and mutators

--- UnitTest.java
+++ UnitTest.java
@@ -20,31 +20,39 @@ public class UnitTest {
 		humanPlayer = new HumanPlayer(-1, 'i');
 		decision = humanPlayer.human(10);
 		org.junit.Assert.assertTrue(decision == 'r' ||decision == 'h');
+		
+		//test human constructor, accessor, and mutator
+		humanPlayer.setHumanScore(10);
+		assertEquals(10, humanPlayer.getHumanScore());
 
 		//test computer decisions
 		ComputerPlayer computerPlayer = new ComputerPlayer(71);
-		decision = computerPlayer.computer(computerPlayer.computerScore.getScore(), 70, 10 );
+		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 10 );
 		assertEquals('r', decision);
 		
 		computerPlayer = new ComputerPlayer(70);
-		decision = computerPlayer.computer(computerPlayer.computerScore.getScore(), 71, 10 );
+		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 71, 10 );
 		assertEquals('r', decision);
 		
 		computerPlayer = new ComputerPlayer(71);
-		decision = computerPlayer.computer(computerPlayer.computerScore.getScore(), 71, 10 );
+		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 71, 10 );
 		assertEquals('r', decision);
 		
 		computerPlayer = new ComputerPlayer(70);
-		decision = computerPlayer.computer(computerPlayer.computerScore.getScore(), 70, 10 );
+		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 10 );
 		assertEquals('r', decision);
 		
 		computerPlayer = new ComputerPlayer(70);
-		decision = computerPlayer.computer(computerPlayer.computerScore.getScore(), 70, 30 );
+		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 30 );
 		assertEquals('h', decision);
 		
 		computerPlayer = new ComputerPlayer(70);
-		decision = computerPlayer.computer(computerPlayer.computerScore.getScore(), 70, 21 );
+		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 21 );
 		assertEquals('h', decision);
+		
+		//test computer constructor, accessor, and mutator
+		computerPlayer.setComputerScore(10);
+		assertEquals(10, computerPlayer.getComputerScore());
 	}
 
 }
