839666a0c839fc119c10b4b0459566215e93beea
dvanleusen <danvanleusen@yahoo.co.uk>
2016-10-14 05:38:43 -0600

    Unit Test after 3rd refactoring
    Tests were revised since parameter objects were introduced; a controller
    test with fixed seed value was added to test human and computer scores

--- UnitTest.java
+++ UnitTest.java
@@ -5,54 +5,60 @@ public class UnitTest {
 	@Test
 	public void TestDecision() {
 		//test human decisions	
-		HumanPlayer humanPlayer = new HumanPlayer(10, 'a');
+		HumanPlayer humanPlayer = new HumanPlayer(10);
 		char decision = humanPlayer.human(9);
 		assertEquals('r', decision);
 		
-		humanPlayer = new HumanPlayer(10, 'a');
 		decision = humanPlayer.human(10);
 		assertEquals('h', decision);
 		
-		humanPlayer = new HumanPlayer(10, 'a');
 		decision = humanPlayer.human(11);
 		assertEquals('h', decision);
 		
-		humanPlayer = new HumanPlayer(-1, 'i');
-		decision = humanPlayer.human(10);
-		org.junit.Assert.assertTrue(decision == 'r' ||decision == 'h');
+		//humanPlayer = new HumanPlayer();
+		//decision = humanPlayer.human(10);
+		//org.junit.Assert.assertTrue(decision == 'r' ||decision == 'h');
 		
 		//test human constructor, accessor, and mutator
 		humanPlayer.setHumanScore(10);
 		assertEquals(10, humanPlayer.getHumanScore());
 
 		//test computer decisions
-		ComputerPlayer computerPlayer = new ComputerPlayer(71);
+		ComputerPlayer computerPlayer = new ComputerPlayer();
+		computerPlayer.setComputerScore(71);
 		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 10 );
 		assertEquals('r', decision);
 		
-		computerPlayer = new ComputerPlayer(70);
+		computerPlayer.setComputerScore(70);
 		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 71, 10 );
 		assertEquals('r', decision);
 		
-		computerPlayer = new ComputerPlayer(71);
+		computerPlayer.setComputerScore(71);
 		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 71, 10 );
 		assertEquals('r', decision);
 		
-		computerPlayer = new ComputerPlayer(70);
+		computerPlayer.setComputerScore(70);
 		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 10 );
 		assertEquals('r', decision);
 		
-		computerPlayer = new ComputerPlayer(70);
+		computerPlayer.setComputerScore(70);
 		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 30 );
 		assertEquals('h', decision);
 		
-		computerPlayer = new ComputerPlayer(70);
+		computerPlayer.setComputerScore(70);
 		decision = computerPlayer.computer(computerPlayer.getComputerScore(), 70, 21 );
 		assertEquals('h', decision);
-		
-		//test computer constructor, accessor, and mutator
-		computerPlayer.setComputerScore(10);
-		assertEquals(10, computerPlayer.getComputerScore());
+	}
+	
+	@Test
+	public void testController() {		
+		//test controller with fixed seed value
+		ComputerPlayer computerPlayer = new ComputerPlayer();
+		HumanPlayer humanPlayer = new HumanPlayer(10);
+		Controller controller = new Controller(computerPlayer, humanPlayer, new Dice(1234));
+		controller.start();
+		assertEquals(37, computerPlayer.getComputerScore());
+		assertEquals(101, humanPlayer.getHumanScore());
 	}
 
 }
